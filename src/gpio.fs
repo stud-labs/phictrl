22f86288414d8376b5ffd13e8affd41b950ec8cc
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-13 13:09:21 +0800

    Refined the loading speed. Added service modules.

--- src/gpio.fs
+++ src/gpio.fs
@@ -1,3 +1,44 @@
+\ Program Name: 1b.fs  for Mecrisp-Stellaris by Matthias Koch and licensed under the GPL
+\ Copyright 2019 t.porter <terry@tjporter.com.au> and licensed under the BSD license.
+\ This program must be loaded before memmap.fs as it provided the pretty printing legend for generic 32 bit prints
+\ Also included is "bin." which prints the binary form of a number with no spaces between numbers for easy copy and pasting purposes
+
+reset
+compiletoram
+
+\ compiletoflash
+
+: b32loop. ( u -- ) \ print 32 bits in 1 bit groups
+0  <#
+31 0 DO
+# 32 HOLD LOOP
+# #>
+TYPE ;
+
+: b32sloop. ( u -- ) \ print 32 bits in 1 bit groups
+ 0  <#
+ 31 0 DO
+ # LOOP
+ # #>
+ TYPE ;
+
+
+: 1b. ( u -- ) cr \ Label 1 bit generic groups
+      @ dup hex. cr
+      ." 3|3|2|2|2|2|2|2|2|2|2|2|1|1|1|1|1|1|1|1|1|1|" cr
+      ." 1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0 " cr
+      binary b32loop. decimal cr
+;
+
+: bin. cr	  \ for manual operations producing easy to paste binary numbers i.e. " 10 bin. "
+       ." 3322222222221111111111" cr
+       ." 10987654321098765432109876543210 " cr
+       binary b32sloop. decimal cr
+;
+
+\ compiletoram
+
+
 
 $40010800 constant GPIOA
 $40010C00 constant GPIOB
@@ -9,12 +50,12 @@ $40010C00 constant GPIOB
 : GPIO.BSRR ( n -- n ) $10 + ;
 : GPIO.BRR ( n -- n ) $14 + ;
 
-: gpio.set ( addr npin out -- n ) 
+: gpio.set ( addr npin out -- n )
   swap \ addr out npin
   2 lshift
   lshift  \ addr code
   swap \ code addr
-  GPIO.CRL bis! 
+  GPIO.CRL bis!
 ;
 
 : .reg ( word -- ) binary . decimal ;
@@ -24,7 +65,3 @@ $40010C00 constant GPIOB
   GPIOA 7 1 gpio.set
   GPIOB 0 1 gpio.set
 ;
-
-
-
-
--- src/term.sh
+++ src/term.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-DELAY=75
+DELAY=100
 
 picocom --imap lfcrlf -b 113200 -s "ascii-xfr -s -l $DELAY -n" /dev/rfcomm0 $*
-
