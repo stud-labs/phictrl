bb47f452614b760eaeb998a86c42c21621affe29
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-14 14:03:16 +0800

    Implementing bit setup

--- src/gpio.fs
+++ src/gpio.fs
@@ -30,7 +30,18 @@ TYPE ;
       binary b32loop. decimal cr
 ;
 
+: CRb. ( u -- ) cr \ Label 1 bit generic groups
+      @ dup hex. cr
+      ." 3|3|2|2|2|2|2|2|2|2|2|2|1|1|1|1|1|1|1|1|1|1|0|0|0|0|0|0|0|0|0|0|" cr
+      ." 1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0|" cr
+      ." ---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+" cr
+      ." c 7|m 7|c 6|m 6|c 5|m 5|c 4|m 4|c 3|m 3|c 2|m 2|c 1|m 1|c 0|m 0|" cr
+      ." ---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+" cr
+      binary b32loop. decimal cr
+;
+
 : bin. cr	  \ for manual operations producing easy to paste binary numbers i.e. " 10 bin. "
+       dup hex. cr
        ." 3322222222221111111111" cr
        ." 10987654321098765432109876543210 " cr
        binary b32sloop. decimal cr
@@ -53,15 +64,21 @@ $40010C00 constant GPIOB
 : gpio.set ( addr npin out -- n )
   swap \ addr out npin
   2 lshift
+  dup
+  $f swap
   lshift  \ addr code
-  swap \ code addr
-  GPIO.CRL bis!
+  .s
+  bin.
+  \ swap \ code addr
+  \ GPIO.CRL bis!
 ;
 
-: .reg ( word -- ) binary . decimal ;
+: reg. ( word -- ) bin. ;
 
 : init.gpios
   GPIOA 6 1 gpio.set
   GPIOA 7 1 gpio.set
   GPIOB 0 1 gpio.set
 ;
+
+: t init.gpios ;
