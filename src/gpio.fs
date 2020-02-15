a2be24acaab60b8d206b7b9701fa4d89047cf518
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-15 11:04:40 +0800

    Implemented blinking with parameters.

--- src/gpio.fs
+++ src/gpio.fs
@@ -1,55 +1,4 @@
-\ Program Name: 1b.fs  for Mecrisp-Stellaris by Matthias Koch and licensed under the GPL
-\ Copyright 2019 t.porter <terry@tjporter.com.au> and licensed under the BSD license.
-\ This program must be loaded before memmap.fs as it provided the pretty printing legend for generic 32 bit prints
-\ Also included is "bin." which prints the binary form of a number with no spaces between numbers for easy copy and pasting purposes
-
 reset
-compiletoram
-
-\ compiletoflash
-
-: b32loop. ( u -- ) \ print 32 bits in 1 bit groups
-0  <#
-31 0 DO
-# 32 HOLD LOOP
-# #>
-TYPE ;
-
-: b32sloop. ( u -- ) \ print 32 bits in 1 bit groups
- 0  <#
- 31 0 DO
- # LOOP
- # #>
- TYPE ;
-
-
-: 1b. ( u -- ) cr \ Label 1 bit generic groups
-      @ dup hex. cr
-      ." 3|3|2|2|2|2|2|2|2|2|2|2|1|1|1|1|1|1|1|1|1|1|" cr
-      ." 1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0 " cr
-      binary b32loop. decimal cr
-;
-
-: CRb. ( u -- ) cr \ Label 1 bit generic groups
-      @ dup hex. cr
-      ." 3|3|2|2|2|2|2|2|2|2|2|2|1|1|1|1|1|1|1|1|1|1|0|0|0|0|0|0|0|0|0|0|" cr
-      ." 1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0|9|8|7|6|5|4|3|2|1|0|" cr
-      ." ---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+" cr
-      ." c 7|m 7|c 6|m 6|c 5|m 5|c 4|m 4|c 3|m 3|c 2|m 2|c 1|m 1|c 0|m 0|" cr
-      ." ---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+" cr
-      binary b32loop. decimal cr
-;
-
-: bin. cr	  \ for manual operations producing easy to paste binary numbers i.e. " 10 bin. "
-       dup hex. cr
-       ." 3322222222221111111111" cr
-       ." 10987654321098765432109876543210 " cr
-       binary b32sloop. decimal cr
-;
-
-\ compiletoram
-
-
 
 $40010800 constant GPIOA
 $40010C00 constant GPIOB
@@ -61,8 +10,9 @@ $40010C00 constant GPIOB
 : GPIO.BSRR ( n -- n ) $10 + ;
 : GPIO.BRR ( n -- n ) $14 + ;
 
-: gpio.set ( addr out npin -- )
+: gpio.set ( addr npin out? -- )
   rot GPIO.CRL >r
+  swap     \ out? npin
 
   2 lshift \ o np*4
   dup      \ o np4 np4
@@ -75,8 +25,9 @@ $40010C00 constant GPIOB
   r> !
 ;
 
-: gpio.out ( GPIO-addr out pin -- )
-  rot GPIO.ODR >r \ out pin
+: gpio.out ( GPIO-addr pin out? -- )
+  rot GPIO.ODR >r \ pin out
+  swap            \ out pin
   dup 1           \ o p p 1
   swap lshift not \ o p 1110111
   r@ @      \ o p 11110111 xxxxx
@@ -89,9 +40,26 @@ $40010C00 constant GPIOB
 : reg. ( word -- ) bin. ;
 
 : init.gpios
-  GPIOA 1 6 gpio.set
-  GPIOA 1 7 gpio.set
-  GPIOB 1 0 gpio.set
+  GPIOA 6 1 gpio.set
+  GPIOA 7 1 gpio.set
+  GPIOB 0 1 gpio.set
 ;
+
+: LED.RED ( -- GPIOA pin ) GPIOA 6 ;
+: LED.GREEN ( -- GPIOA pin ) GPIOA 7 ;
+: LED.BLUE ( -- GPIOB pin ) GPIOB 0 ;
+
+: led.on ( GPIOX pin -- ) 1 gpio.out ;
+: led.off ( GPIOX pin -- ) 0 gpio.out ;
+
+: led.red.on LED.RED led.on ;
+: led.red.off LED.RED led.off ;
+: led.green.on LED.GREEN led.on ;
+: led.green.off LED.GREEN led.off ;
+: led.blue.on LED.BLUE led.on ;
+: led.blue.off LED.BLUE led.off ;
+
+: led.sleep 150000 0 do nop loop ;  ok.
+: blink.test ( GPIOX pin -- ) begin 2dup led.on led.sleep 2dup led.off led.sleep key? until 2drop ;
 
 : t init.gpios ;
