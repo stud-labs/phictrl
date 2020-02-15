b9c2faf3b3e7b5a5656ac8a39aec3f3cbb049296
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-15 12:20:00 +0800

    Implementing IR input.

--- src/gpio.fs
+++ src/gpio.fs
@@ -25,7 +25,7 @@ $40010C00 constant GPIOB
   r> !
 ;
 
-: gpio.out ( GPIO-addr pin out? -- )
+: gpio.out ( GPIOX pin out? -- )
   rot GPIO.ODR >r \ pin out
   swap            \ out pin
   dup 1           \ o p p 1
@@ -37,17 +37,20 @@ $40010C00 constant GPIOB
   r> !
 ;
 
-: reg. ( word -- ) bin. ;
-
-: init.gpios
-  GPIOA 6 1 gpio.set
-  GPIOA 7 1 gpio.set
-  GPIOB 0 1 gpio.set
+: gpio.in ( GPIOX pin -- 0|1 )
+  swap GPIO.IDR @ \ pin xxxvxxx
+  over \ pin xvx pin
+  1 swap \ pin xvx 1 pin
+  lshift and \ pin 000v000
+  swap rshift \ 00000v
 ;
+
+: reg. ( word -- ) bin. ;
 
 : LED.RED ( -- GPIOA pin ) GPIOA 6 ;
 : LED.GREEN ( -- GPIOA pin ) GPIOA 7 ;
 : LED.BLUE ( -- GPIOB pin ) GPIOB 0 ;
+: IR.IC ( -- GPIOA pin ) GPIOA 3 ;
 
 : led.on ( GPIOX pin -- ) 1 gpio.out ;
 : led.off ( GPIOX pin -- ) 0 gpio.out ;
@@ -62,4 +65,18 @@ $40010C00 constant GPIOB
 : led.sleep 150000 0 do nop loop ;  ok.
 : blink.test ( GPIOX pin -- ) begin 2dup led.on led.sleep 2dup led.off led.sleep key? until 2drop ;
 
+: init.gpios
+  LED.RED 1 gpio.set
+  LED.GREEN 1 gpio.set
+  LED.BLUE 1 gpio.set
+  IR.IC 0 gpio.set
+;
+
 : t init.gpios ;
+: t.ir
+  init.gpios base @
+  begin
+    IR.IC gpio.in . key?
+  until
+  base !
+;
