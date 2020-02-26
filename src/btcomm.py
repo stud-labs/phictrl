832a766d2873e77becfed79505527dc75fd98fba
Evgeny Cherkashin <eugeneai@irnok.net>
2020-02-26 14:11:14 +0800

    Added example for bt connection.

---
+++ src/btcomm.py
@@ -0,0 +1,25 @@
+import socket
+
+
+controller = '00:15:83:12:12:E7'  # <adapter address>
+device = '00:19:09:03:5A:AE'  # <device address>
+
+
+def conn():
+    sock = socket.socket(socket.AF_BLUETOOTH,
+                         socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    sock.bind((controller, 1))
+    sock.connect((device, 1))
+    # If not pinned it will ask you. You can use/adapt the bluez simple-agent for headless pinning
+    return sock
+
+
+def main():
+    sock = conn()
+    sock.send(b'hello\n')
+    print("Received: ", sock.recv(100))
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
